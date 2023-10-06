3507e45d976a45b77cb2cffb9fbb24570f0b6f54
yeo311 <jihoonyeo311@gmail.com>
2023-10-06 10:52:51 +0900

    feat: Loading Skeleton 추가

--- src/app/[year]/[month]/loading.tsx
+++ src/app/[year]/[month]/loading.tsx
@@ -1,12 +1,48 @@
-import Spinner from '@/components/Spinner';
+import { PropsWithChildren } from 'react';
+import Skeleton from 'react-loading-skeleton';
+
+function LoadingItemSection({ children }: PropsWithChildren) {
+  return <div className="mt-2">{children}</div>;
+}
+
+function LoadingItemRow({ children }: PropsWithChildren) {
+  return <div className="flex py-2">{children}</div>;
+}
+
+function LoadingItemList() {
+  return (
+    <LoadingItemSection>
+      <Skeleton
+        style={{
+          padding: '0.25rem 0px',
+        }}
+      />
+      {Array.from({ length: 4 }).map((_, i) => (
+        <LoadingItemRow key={i}>
+          <Skeleton
+            circle
+            containerClassName="w-10"
+            style={{ height: '2.5rem' }}
+          />
+          <Skeleton
+            count={2}
+            containerClassName="flex-1"
+            style={{ marginLeft: '0.375rem' }}
+          />
+        </LoadingItemRow>
+      ))}
+    </LoadingItemSection>
+  );
+}
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center w-full h-[100vh] z-10">
-      <Spinner />
-      <div
-        className={`fixed w-full h-full left-0 top-0 bg-black opacity-10 z-10`}
-      />
+    <div>
+      <Skeleton height={36} width={92} style={{ marginBottom: '12px' }} />
+      <Skeleton height={28} width={150} />
+      <Skeleton height={28} width={150} />
+      <LoadingItemList />
+      <LoadingItemList />
     </div>
   );
 }
